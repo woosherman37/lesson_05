ea552c72b96038e8ee51d053a63934c7c3277016
ShermanWoo <sherman@ShermanWoos-MacBook-Air.local>
2024-11-14 16:37:48 +0000

    Fixed Bug: Num > 1

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ x = input(f"\nJust give me a number and I'll show you I can count\n")
 
 x = int(x)
 
+if x <= 1:
+	x = input(f"\nJust give me a number higher than 1 please\n")
+	x = int(x) 
+
 print(f"\nOk, thanks. I will start from 1 and stop at {x}\n")
 
 for num in range(1, x):
